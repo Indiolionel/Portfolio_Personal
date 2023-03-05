13d0a834804a70f5da117cce18ac2bf579582053
Santi <santi.cocciardi@gmail.com>
2023-03-04 21:02:35 -0300

    page projects, new proects

--- my-app/src/components/Projects.js
+++ my-app/src/components/Projects.js
@@ -2,21 +2,32 @@
 import { CodeIcon } from "@heroicons/react/solid";
 import React from "react";
 import photoPlantas from '../img/proyectoPlantas.png'
+import todoList from '../img/todoList.png'
+import trueque from '../img/trueque.png'
 
 // import { projects } from "../data";
 
 export default function Projects() {
 
-  const projects = [{
-
-    link: "https://react-nucba-production.up.railway.app/",
-    gitLink: "https://github.com/Indiolionel/React-Nucba",
-    image: `${photoPlantas}`,
-    subtitle: "App venta de plantas",
-    title: "Plantas App",
-    description: "Desarrollo Full-Stack"
+  const projects = [
+    {
+      link: "https://integrador-trueque.vercel.app/",
+      gitLink: "https://github.com/Indiolionel/integrador-trueque",
+      image: `${trueque}`,
+      subtitle: "App hecha solo con js vanilla",
+      title: "App truequeLand",
+      description: "Desarrollo Front-End"
+    },
+  {
+    link: "https://to-do-list-five-lime.vercel.app/",
+    gitLink: "https://github.com/Indiolionel/ToDoList",
+    image: `${todoList}`,
+    subtitle: "App conexion api pokeapi y lista de tareas",
+    title: "App to-do-list",
+    description: "Desarrollo Front-End"
   },
   {
+
     link: "https://react-nucba-production.up.railway.app/",
     gitLink: "https://github.com/Indiolionel/React-Nucba",
     image: `${photoPlantas}`,
@@ -40,20 +51,20 @@ export default function Projects() {
         <div className="flex flex-wrap justify-center -m-4">
           {projects.map((project) => (
             <>
-              <div className="mx-2">
+              <div className="mx-2 border-2 border-gray-800 my-2 bg-black">
                 <a
                   href={project.link}
                   key={project.image}
                   className="sm:w-1/2 w-100 p-4"
                   target="_blank"
                 >
-                  <div className="flex relative mb-2">
+                  <div className="flex relative mb-2 w-64 h-64">
                     <img
                       alt="Plantas-img"
                       className="absolute inset-0 w-full h-full object-cover object-center"
                       src={project.image}
                     />
-                    <div className="px-8 py-10 relative z-10 w-full border-4 border-gray-800 bg-gray-900 opacity-0 hover:opacity-100">
+                    <div className="px-8 py-10 relative z-10 w-full border-y border-gray-700 bg-gray-900 opacity-0 hover:opacity-100">
                       <h2 className="tracking-widest text-sm title-font font-medium text-green-400 mb-1">
                         {project.subtitle}
                       </h2>
@@ -64,7 +75,7 @@ export default function Projects() {
                     </div>
                   </div>
                 </a>
-                <span className="rounded bg-green-700 hover:bg-gray-700 "><a className="p-4 text-white" href={project.gitLink} target="_blank">Proyecto en Git</a></span>
+                <a class="inline-flex items-center justify-center h-10 px-5 py-3 my-2 border border-transparent text-base font-medium rounded-md text-white bg-gray-700 hover:bg-gray-800" href={project.gitLink}> Proyecto en Git</a>
               </div>
             </>
           ))}
